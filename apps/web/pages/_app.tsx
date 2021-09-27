e21c65657990958dec6578f3a06b7c5e33d8b10d
Hieu Tran <hieutran.fu@gmail.com>
2021-09-27 22:38:33 +0700

    setup urql auth dev

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -2,8 +2,91 @@ import { NavigationBar } from 'components/NavigationBar';
 import { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 import dynamic from 'next/dynamic';
+import { createClient, Provider } from 'urql';
+import { makeOperation } from '@urql/core';
+import { authExchange } from '@urql/exchange-auth';
 import Head from 'next/head';
 import '../styles.css';
+
+type AuthState = {
+  accessToken: string;
+  refreshToken: string;
+};
+type GetAuthParams = {
+  authState: AuthState;
+  mutate: any;
+};
+
+const addAuthToOperation = ({ authState, operation }) => {
+  if (!authState || !authState.token) {
+    return operation;
+  }
+
+  const fetchOptions =
+    typeof operation.context.fetchOptions === 'function'
+      ? operation.context.fetchOptions()
+      : operation.context.fetchOptions || {};
+
+  return makeOperation(operation.kind, operation, {
+    ...operation.context,
+    fetchOptions: {
+      ...fetchOptions,
+      headers: {
+        ...fetchOptions.headers,
+        Authorization: authState.token,
+      },
+    },
+  });
+};
+
+const getAuth = async ({
+  authState,
+  mutate,
+}: GetAuthParams): Promise<AuthState | null> => {
+  if (!authState) {
+    const accessToken = localStorage.getItem('accessToken');
+    const refreshToken = localStorage.getItem('refreshToken');
+    if (accessToken && refreshToken) {
+      return { accessToken, refreshToken };
+    }
+    return null;
+  }
+
+  const result = await mutate(
+    `
+    mutation RefreshToken($token: String!) {
+      refreshToken(token: $token) {
+        accessToken
+        refreshToken
+      }
+    }`,
+    {
+      token: authState!.refreshToken,
+    }
+  );
+
+  if (result.data?.refreshLogin) {
+    localStorage.setItem('token', result.data.refreshLogin.token);
+    localStorage.setItem('refreshToken', result.data.refreshLogin.refreshToken);
+
+    return {
+      accessToken: result.data.refreshLogin.accessToken,
+      refreshToken: result.data.refreshLogin.refreshToken,
+    };
+  }
+
+  // This is where auth has gone wrong and we need to clean up and redirect to a login page
+  localStorage.clear();
+  // redirect to sign in page
+  // logout();
+
+  return null;
+};
+
+const client = createClient({
+  url: 'http://localhost:3000/graphql',
+  exchanges: [authExchange({ addAuthToOperation, getAuth })],
+});
 
 const LayoutAdmin = dynamic(() => import('layouts/admin/LayoutAdmin'), {
   ssr: false,
@@ -24,11 +107,11 @@ export default function App({ Component, pageProps }: AppProps) {
     </>
   );
   return (
-    <>
+    <Provider value={client}>
       <Head>
         <title>Welcome to web!</title>
       </Head>
       {node}
-    </>
+    </Provider>
   );
 }
--- apps/web/pages/auth/sign_up.tsx
+++ apps/web/pages/auth/sign_up.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { FormControl } from 'components/FormControl';
 import { useForm, SubmitHandler, useFormState } from 'react-hook-form';
+import { useMutation } from 'urql';
 
 type SignUpFormData = {
   username: string;
@@ -11,16 +12,34 @@ type SignUpFormData = {
   confirmPassword: string;
 };
 
+const SignUpMutation = `
+mutation SignUp($input: SignUpInput!) {
+  signUp(input: $input) {
+    accessToken
+    refreshToken
+  }
+}
+`;
+
 const SignUp: React.FunctionComponent = () => {
   const {
     register,
     handleSubmit,
     watch,
     control,
+    getValues,
     formState: { errors },
-  } = useForm<SignUpFormData>();
-  const onSubmit: SubmitHandler<SignUpFormData> = (data) => console.log(data);
+  } = useForm<SignUpFormData>({
+    mode: 'onTouched',
+  });
   const { isValid } = useFormState({ control });
+  const [signUpResult, signUp] = useMutation(SignUpMutation);
+  const onSubmit: SubmitHandler<SignUpFormData> = async ({
+    confirmPassword,
+    ...input
+  }) => {
+    const result = await signUp({ input });
+  };
   return (
     <div className="max-w-md mx-auto px-4 sm:px-6 py-4 bg-white shadow-sm rounded-md mt-6">
       <h3 className="text-center font-medium text-xl mb-4">Sign up Viforo</h3>
@@ -33,28 +52,53 @@ const SignUp: React.FunctionComponent = () => {
             minLength: 6,
             maxLength: 15,
           })}
+          error={
+            errors.username
+              ? 'Username length must be from 6 to 15 characters'
+              : undefined
+          }
         />
+
         <FormControl
           label="Email address"
           type="email"
           autoComplete="email"
           required
           {...register('email', {
-            required: true,
+            required: {
+              value: true,
+              message: 'Email is a required field',
+            },
           })}
+          error={errors.email?.message}
         />
 
         <FormControl
           label="First name"
           required
           {...register('firstName', {
-            required: true,
-            maxLength: 32,
+            required: {
+              value: true,
+              message: 'First name is a required field',
+            },
+            maxLength: {
+              value: 32,
+              message:
+                'First name length must be not greater than 32 characters',
+            },
           })}
+          error={errors.firstName?.message}
         />
         <FormControl
           label="Last name"
-          {...register('lastName', { maxLength: 32 })}
+          {...register('lastName', {
+            maxLength: {
+              value: 32,
+              message:
+                'Last name length must be not greater than 32 characters',
+            },
+          })}
+          error={errors.lastName?.message}
         />
 
         <FormControl
@@ -63,8 +107,12 @@ const SignUp: React.FunctionComponent = () => {
           autoComplete="current-password"
           required
           {...register('password', {
-            required: true,
+            required: {
+              value: true,
+              message: 'Password is a required field',
+            },
           })}
+          error={errors.password?.message}
         />
 
         <FormControl
@@ -72,14 +120,28 @@ const SignUp: React.FunctionComponent = () => {
           type="password"
           required
           {...register('confirmPassword', {
-            required: true,
+            required: {
+              value: true,
+              message: 'Please enter the confirmation password',
+            },
+            validate: (valValue) => {
+              return valValue === getValues('password')
+                ? true
+                : 'Password and confirmation password must be the same';
+            },
           })}
+          error={errors.confirmPassword?.message}
         />
+        {signUpResult.error?.message && (
+          <div className="text-white px-6 py-4 border-0 rounded relative bg-pink-500">
+            {signUpResult.error?.message}
+          </div>
+        )}
 
         <button
           type="submit"
-          className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50"
           disabled={!isValid}
+          className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50"
         >
           Sign up
         </button>
